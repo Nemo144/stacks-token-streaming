96efa87ae37268694e55cd642bc63feac834fb20
Nnamdi Okeke <onnamdialex@gmail.com>
2025-06-17 17:50:22 +0100

    :white_check_mark: Add test for the test token streaming contract

--- tests/stream.test.ts
+++ tests/stream.test.ts
@@ -1,21 +1,36 @@
+import {
+  Cl,
+  createStacksPrivateKey,
+  cvToValue,
+  signMessageHashRsv,
+} from "@stacks/transactions";
+import { beforeEach, describe, expect, it } from "vitest";
 
-import { describe, expect, it } from "vitest";
-
+// `simnet` is a simulation network
 const accounts = simnet.getAccounts();
-const address1 = accounts.get("wallet_1")!;
 
-/*
-  The test below is an example. To learn more, read the testing documentation here:
-  https://docs.hiro.so/stacks/clarinet-js-sdk
-*/
+//the identifiers of this wallets can be found in the settings/devnet.toml config file
+const sender = accounts.get("wallet_1")!;
+const recipient = accounts.get("wallet_2")!;
+const randomUser = accounts.get("wallet_3")!;
 
-describe("example tests", () => {
-  it("ensures simnet is well initalised", () => {
-    expect(simnet.blockHeight).toBeDefined();
+describe("test token streaming contract", () => {
+  //before each test is run, we want to create a stream
+  //so we can run the test around different possible things to do with the stream
+  beforeEach(() => {
+    const result = simnet.callPublicFn(
+      "stream",
+      "stream-to",
+      [
+        Cl.principal(recipient),
+        Cl.uint(5),
+        Cl.tuple({ "start-block": Cl.uint(0), "stop-block": Cl.uint(5) }),
+        Cl.uint(1),
+      ],
+      sender
+    );
+    expect(result.events[0].event).toBe("stx_transfer_event");
+    expect(result.events[0].data.amount).toBe("5");
+    expect(result.events[0].data.sender).toBe(sender);
   });
-
-  // it("shows an example", () => {
-  //   const { result } = simnet.callReadOnlyFn("counter", "get-counter", [], address1);
-  //   expect(result).toBeUint(0);
-  // });
 });
